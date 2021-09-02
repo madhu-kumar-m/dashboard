308771a1951963d8a0df2fefbd9eb6f99f0cf87a
madhu-kumar-m <madhukumar0006@gmail.com>
2021-09-02 11:22:38 +0530

    added pages component and category-dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavigationMenuComponent } from './components/navigation-menu/navigation-menu.component';
+import { CategoryDashboardComponent } from './components/pages/category-dashboard/category-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationMenuComponent,
+    CategoryDashboardComponent
   ],
   imports: [
     BrowserModule,
