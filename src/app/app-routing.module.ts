fa013c48bf0b999f9b4b923c05d6c8caf42cea30
madhu-kumar-m <madhukumar0006@gmail.com>
2021-09-12 15:02:43 +0530

    made changes and project is stable, graph, shares increase/decrease and responsiveness is working good

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryDashboardComponent } from './components/pages/category-dashboard/category-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CategoryDashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
