417e7a7ac0810c4c43f18999fbf6e6120922b604
madhu-kumar-m <madhukumar0006@gmail.com>
2021-09-13 12:40:47 +0530

    Fixed resizing issue and showing up side bar on reload, I have added a function code for that on reload sidebar should show up if user expands from small screen to large screen without side bar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,11 +9,30 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   showMe:boolean = true;
+  window: any;
 
   constructor() { }
 
   ngOnInit(): void {
+
+    var screen_size:number|undefined = $(window).width();
+    if (screen_size == undefined){
+      this.showMe = false;
+    } else {
+      if(screen_size < 830)
+    {
+      console.log(screen_size + "true")
+      this.showMe = false;
+    } else {
+      console.log(screen_size + "false")
+      this.showMe = true;
+    }
+    }
+
+
   }
+
+
 
   toggleMe(){
     this.showMe =!this.showMe;
